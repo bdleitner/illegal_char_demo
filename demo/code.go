c905ac1f08caa569e5053d7218c7959187a32e3f
Ben Leitner <7515022+bdleitner@users.noreply.github.com>
2022-10-26 18:35:38 -0700

    initial commit

---
+++ demo/code.go
@@ -0,0 +1,5 @@
+package demo
+
+func simple(n int) int {
+	return n << 1
+}
---
+++ demo/code_test.go
@@ -0,0 +1,17 @@
+package demo
+
+import (
+	"testing"
+)
+
+func TestSimplePass(t *testing.T) {
+	if got := simple(2); got != 4 {
+		t.Errorf("Got %d wanted 4", got)
+	}
+}
+
+//func TestSimpleFail(t *testing.T) {
+//  if got := simple(2); got != 5 {
+//    t.Errorf("Got %d wanted 5", got)
+//  }
+//}
